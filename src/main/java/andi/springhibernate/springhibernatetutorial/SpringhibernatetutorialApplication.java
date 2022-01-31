bd4c86e5c3d304c71f33566df87b7d686b4d3356
Andi <Andi@Andis-Air.fritz.box>
2022-01-31 21:32:05 +0100

    project skeleton

---
+++ src/main/java/andi/springhibernate/springhibernatetutorial/SpringhibernatetutorialApplication.java
@@ -0,0 +1,13 @@
+package andi.springhibernate.springhibernatetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringhibernatetutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringhibernatetutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andi/springhibernate/springhibernatetutorial/SpringhibernatetutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package andi.springhibernate.springhibernatetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringhibernatetutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
